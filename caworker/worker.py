7ee4cc717d4e4deba7ef7d7b68172734f4dd1754
gpcortes <gpcortes@gmail.com>
2022-12-07 13:45:50 -0300

    fetch_tasks return tuble

--- caworker/worker.py
+++ caworker/worker.py
@@ -1,12 +1,14 @@
 ﻿import platform
 import pycamunda.externaltask
 import pycamunda.processinst
+import pycamunda.task
 import requests.auth
 import requests.sessions
 import envconfiguration as config
 
 
 class Worker:
+
     def __init__(self):
         self.ENG_REST_URL = config.ENG_REST_URL
         self.HOST = platform.node()
@@ -15,24 +17,23 @@ class Worker:
         self.ENG_REST_USERNAME = config.ENG_REST_USERNAME
         self.ENG_REST_PASSWORD = config.ENG_REST_PASSWORD
         self.MAX_TASK_DURATION = config.MAX_TASK_DURATION
-        self.SESSION = self.__get_auth(
-              self.ENG_REST_USERNAME, 
-              self.ENG_REST_PASSWORD
-            )
+        self.SESSION = self.__get_auth(self.ENG_REST_USERNAME,
+                                       self.ENG_REST_PASSWORD)
 
     def __get_auth(self, username, password):
         session = requests.sessions.Session()
-        session.auth = requests.auth.HTTPBasicAuth(
-            username=username, password=password)
+        session.auth = requests.auth.HTTPBasicAuth(username=username,
+                                                   password=password)
         return session
 
     def fetch_tasks(self, topic=None, max_tasks=1, lock_duration=30000):
-        
+
         topic = self.TOPIC if topic is None else topic
-        
+
         fetch_and_lock = pycamunda.externaltask.FetchAndLock(
-            url=self.ENG_REST_URL, worker_id=self.WORKER_ID, max_tasks=max_tasks
-        )
+            url=self.ENG_REST_URL,
+            worker_id=self.WORKER_ID,
+            max_tasks=max_tasks)
 
         fetch_and_lock.add_topic(name=topic, lock_duration=lock_duration)
         fetch_and_lock.session = self.SESSION
@@ -47,16 +48,16 @@ class Worker:
             return tuple()
 
     def complete_task(self, task_id, variables={}):
-        complete = pycamunda.externaltask.Complete(
-            url=self.ENG_REST_URL, id_=task_id, worker_id=self.WORKER_ID)
+        complete = pycamunda.externaltask.Complete(url=self.ENG_REST_URL,
+                                                   id_=task_id,
+                                                   worker_id=self.WORKER_ID)
         complete.session = self.SESSION
 
         for variable in variables:
-            complete.add_variable(
-                name=variables[variable]['name'], 
-                value=variables[variable]['value'],
-                type_=variables[variable]['type'] if 'type' in variables[variable] else 'string'
-            )
+            complete.add_variable(name=variables[variable]['name'],
+                                  value=variables[variable]['value'],
+                                  type_=variables[variable]['type'] if 'type'
+                                  in variables[variable] else 'string')
 
         try:
             return complete()
