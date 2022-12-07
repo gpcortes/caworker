00a906f8e193c8a9c9399a1bcc77e6964922fda3
gpcortes <gpcortes@gmail.com>
2022-12-07 14:39:35 -0300

    add envconfiguration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='caworker',
-    version='0.1.3',
+    version='0.1.4',
     description='Camunda Worker',
     url='https://github.com/gpcortes/caworker.git',
     author='Gustavo Côrtes',
